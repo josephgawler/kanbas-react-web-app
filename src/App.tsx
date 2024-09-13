2a865ddd7ac92c103fa50969e503b8fe92b348ee
Joseph Gawler <joey@Josephs-MacBook-Air.local>
2024-09-13 16:16:01 -0400

    my first commit

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Labs from './Labs';
+import Lab1 from './Labs/Lab1';
+
+function App() {
+  return (
+    <div>
+      <h1>Welcome to Web Dev</h1>
+      <Labs />
+      <Lab1 />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Labs.tsx
@@ -0,0 +1,9 @@
+export default function Labs() {
+    return (
+        <div>
+            <h1>Labs</h1>
+            <h2>Lab 1</h2>
+            <h3>Exercise 1.1</h3>
+        </div>
+    );
+}
