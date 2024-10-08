952f813ac76bf5cd275395a5dff469b3dc8fdd0e
Joseph Gawler <joey@Josephs-MacBook-Air.local>
2024-10-08 11:15:31 -0400

    Updates up to 4.7 edit assignment screen

--- src/Kanbas/Courses/Assignments/Editor.tsx
+++ src/Kanbas/Courses/Assignments/Editor.tsx
@@ -1,6 +1,28 @@
 export default function AssignmentEditor() {
     return (
       <div id="wd-assignments-editor">
+        <div className="mb-3 row col-sm-10 wd-padded-left-form">
+            <label htmlFor="assignmentname"
+                className="col-sm-10 col-form-label">
+            Assignment Name </label> <br></br>
+            
+            <input type="text" id="assignmentname"
+                    className="form-control" />
+
+
+            </div>
+          <div className="mb-3 row col-sm-10 wd-padded-left-form">
+          
+            <label htmlFor="description"
+                className="col-sm-10 col-form-label">
+            Description </label>
+            <div className="col-sm-10">
+            <textarea className="form-control"
+                id="description" rows={3}></textarea>
+            </div>
+        
+          </div>
+          
         <h3>
         <label htmlFor="wd-name">Assignment Name</label><br></br>
         </h3>
