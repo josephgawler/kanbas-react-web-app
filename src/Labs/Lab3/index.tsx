021ddde9bb5a3a3a32deabb193151ba8b38f921d
Joseph Gawler <joey@Josephs-MacBook-Air.local>
2024-10-21 23:32:31 -0400

    Adding up to 2.7.1

--- src/Labs/Lab3/index.tsx
+++ src/Labs/Lab3/index.tsx
@@ -17,7 +17,23 @@ import MapFunction from "./MapFunction";
 import FindFunction from "./FindFunction";
 import FindIndex from "./FindIndex";
 import FilterFunction from "./FilterFunction";
+import JsonStringify from "./JsonStringify";
+import House from "./House";
+import TodoItem from "./todo/TodoItem";
+import TodoList from "./todo/TodoList";
+import Spreading from "./Spreading";
+import Destructing from "./Destructing";
+import FunctionDestructing from "./FunctionDestructing";
+import DestructingImports from "./DestructingImports";
+import Classes from "./Classes";
+import Styles from "./Styles";
+import Add from "./Add";
+import Square from "./Square";
+import Highlight from "./Highlight";
+import PathParameters from "./PathParameters";
+import AddPathParameters from "./AddPathParameters";
 export default function Lab3() {
+    console.log('Hello World');
     return (
       <div id="wd-lab3">
         <h3>Lab 3</h3>
@@ -40,6 +56,27 @@ export default function Lab3() {
         <FindFunction/>
         <FindIndex/>
         <FilterFunction/>
+        <JsonStringify/>
+        <House/>
+        <TodoItem/>
+        <TodoList/>
+        <Spreading/>
+        <Destructing/>
+        <FunctionDestructing/>
+        <DestructingImports/>
+        <Classes/>
+        <Styles/>
+        <Add a={3} b={4} />
+        <h4>Square of 4</h4>
+        <Square>4</Square>
+        <hr />
+        <Highlight>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipitratione eaque illo minus cum, saepe totam
+        vel nihil repellat nemo explicabo excepturi consectetur. Modi omnis minus sequi maiores, provident voluptates.
+        </Highlight>
+        <hr />
+        <PathParameters/>
+        
       </div>
     );
   }
